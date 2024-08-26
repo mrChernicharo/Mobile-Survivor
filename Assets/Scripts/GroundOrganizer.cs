cfe2e722c9bcac1fb850df03395d32da0fd6147d
Felipe Chernicharo <Felipe.Melo_Chernicharo@genesys.com>
2024-08-26 13:46:58 -0300

    replace int Arrays with Quad Lists

--- Assets/Scripts/GroundOrganizer.cs
+++ Assets/Scripts/GroundOrganizer.cs
@@ -5,61 +5,96 @@ using UnityEngine;
 
 public class GroundOrganizer : MonoBehaviour
 {
-    private int itemCount = 0;
-    private int[][] matrix = new int[][] {
-        new int[] { 0, 1, 2 },
-        new int[] { 3, 4, 5 },
-        new int[] { 6, 7, 8 }
-    };
+    public GameObject quadPrefab;
+    public int rows = 3;
+    public int columns = 3;
+
+    private float quadSize = 16f;
+    private List<List<GameObject>> quadGrid = new List<List<GameObject>>();
+
 
     void Start()
     {
-        for (int i = 0; i < matrix.Length; i++)
+        CreateQuadGrid();
+    }
+
+    void CreateQuadGrid()
+    {
+
+        List<List<Vector3>> positions = new List<List<Vector3>>();
+        for (int i = 1; i > -2; i--)
         {
-            for (int j = 0; j < matrix[0].Length; j++)
+            List<Vector3> row = new List<Vector3>();
+            for (int j = -1; j < 2; j++)
             {
-                itemCount++;
+                Vector3 pos = new Vector3(j * quadSize, i * quadSize, 1f);
+                row.Add(pos);
             }
+            positions.Add(row);
+        }
+
+
+        for (int i = 0; i < rows; i++)
+        {
+            List<GameObject> row = new List<GameObject>();
+            for (int j = 0; j < columns; j++)
+            {
+                GameObject newQuad = Instantiate(quadPrefab, transform);
+                newQuad.transform.position = positions[i][j];
+                newQuad.name = $"Quad {(i * 3) + j}";
+                // Create a new Material with a random color
+                Material newMaterial = new Material(quadPrefab.GetComponent<Renderer>().sharedMaterial);
+                newMaterial.color = UnityEngine.Random.ColorHSV();
+                newQuad.GetComponent<Renderer>().material = newMaterial;
+
+                row.Add(newQuad);
+            }
+            quadGrid.Add(row);
+        }
+        Debug.Log("quadGrid created");
+        Debug.Log(quadPrefab);
+    }
+
+    [ContextMenu("DebugQuadGrid")]
+    void DebugQuadGrid()
+    {
+        for (int i = 0; i < rows; i++)
+        {
+            string rowString = "[ ";
+            for (int j = 0; j < columns; j++)
+            {
+                rowString += quadGrid[i][j].name;
+                if (j < 2) rowString += ", ";
+                else rowString += " ";
+            }
+            rowString += " ]";
+            Debug.Log(rowString);
         }
     }
 
 
-    void PrintMatrix()
-    {
-        Debug.Log($"0 -> [ {string.Join(", ", matrix[0])} ]");
-        Debug.Log($"1 -> [ {string.Join(", ", matrix[1])} ]");
-        Debug.Log($"2 -> [ {string.Join(", ", matrix[2])} ]");
-        Debug.Log($"========================================");
-    }
 
-    [ContextMenu("GetMatrix")]
-    int[][] GetMatrix()
-    {
-        PrintMatrix();
-        return matrix;
-    }
 
-    [ContextMenu("ResetMatrix")]
-    void ResetMatrix()
-    {
-        matrix = new int[][] {
-            new int[] { 0, 1, 2 },
-            new int[] { 3, 4, 5 },
-            new int[] { 6, 7, 8 }
-        };
-        PrintMatrix();
-    }
+    // [ContextMenu("ResetMatrix")]
+    // void ResetMatrix()
+    // {
+    //     matrix = new int[][] {
+    //         new int[] { 0, 1, 2 },
+    //         new int[] { 3, 4, 5 },
+    //         new int[] { 6, 7, 8 }
+    //     };
+    // }
 
 
     [ContextMenu("MoveTopDown")]
     void MoveTopDown()
     {
         // teleport top row to bottom
-        int[] temp = matrix[0];
-        matrix[0] = matrix[1];
-        matrix[1] = matrix[2];
-        matrix[2] = temp;
-        PrintMatrix();
+        List<GameObject> temp = quadGrid[0];
+        quadGrid[0] = quadGrid[1];
+        quadGrid[1] = quadGrid[2];
+        quadGrid[2] = temp;
+        DebugQuadGrid();
     }
 
 
@@ -67,39 +102,39 @@ public class GroundOrganizer : MonoBehaviour
     void MoveBottomUp()
     {
         // teleport bottom row to the top
-        int[] temp = matrix[2];
-        matrix[2] = matrix[1];
-        matrix[1] = matrix[0];
-        matrix[0] = temp;
-        PrintMatrix();
+        List<GameObject> temp = quadGrid[2];
+        quadGrid[2] = quadGrid[1];
+        quadGrid[1] = quadGrid[0];
+        quadGrid[0] = temp;
+        DebugQuadGrid();
     }
 
     [ContextMenu("MoveLeftRight")]
     void MoveLeftRight()
     {
         // teleport leftmost row to right side
-        for (int i = 0; i < matrix.Length; i++)
+        for (int i = 0; i < quadGrid.Count; i++)
         {
-            int temp = matrix[i][0];
-            matrix[i][0] = matrix[i][1];
-            matrix[i][1] = matrix[i][2];
-            matrix[i][2] = temp;
+            GameObject temp = quadGrid[i][0];
+            quadGrid[i][0] = quadGrid[i][1];
+            quadGrid[i][1] = quadGrid[i][2];
+            quadGrid[i][2] = temp;
         }
-        PrintMatrix();
+        DebugQuadGrid();
     }
 
     [ContextMenu("MoveRightLeft")]
     void MoveRightLeft()
     {
         // teleport rightmost row to left side
-        for (int i = 0; i < matrix.Length; i++)
+        for (int i = 0; i < quadGrid.Count; i++)
         {
-            int temp = matrix[i][2];
-            matrix[i][2] = matrix[i][1];
-            matrix[i][1] = matrix[i][0];
-            matrix[i][0] = temp;
+            GameObject temp = quadGrid[i][2];
+            quadGrid[i][2] = quadGrid[i][1];
+            quadGrid[i][1] = quadGrid[i][0];
+            quadGrid[i][0] = temp;
         }
-        PrintMatrix();
+        DebugQuadGrid();
     }
 
 }
